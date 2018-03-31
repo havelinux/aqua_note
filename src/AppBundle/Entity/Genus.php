dca03b284a4049881b9a1b5d7f965319b21254ab
abel verbo <havelinux@gmail.com>
2018-03-31 19:38:24 +0200

    add dummy data w alice

--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -38,6 +38,11 @@ class Genus
   private $funFact;
 
   /**
+   * @ORM\Column(type="boolean")
+   */
+  private $isPublished;
+
+  /**
    * @return mixed
    */
   public function getName()
@@ -104,6 +109,14 @@ class Genus
   public function getUpdatedAt()
   {
     return new \DateTime('-'.rand(0, 100).' days');
+  }
+
+  /**
+   * @param mixed $isPublished
+   */
+  public function setIsPublished($isPublished)
+  {
+    $this->isPublished = $isPublished;
   }
 
 }
