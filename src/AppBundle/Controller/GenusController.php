796d65623417852fcbb03f7c6904612fd716d991
abel verbo <havelinux@gmail.com>
2018-03-31 13:20:40 +0200

    add action

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -37,6 +37,16 @@ class GenusController extends Controller
   }
 
   /**
+   * @Route("/genus")
+   */
+  public function listAction()
+  {
+    $em = $this->getDoctrine()->getManager();
+    $genuses = $em->getRepository('AppBundle:Genus')->findAll();
+    dump($genuses);die;
+  }
+
+  /**
    * @Route("/genus/{genusName}")
    */
   public function showAction($genusName)
