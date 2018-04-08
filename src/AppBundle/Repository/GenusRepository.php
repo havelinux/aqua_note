af14a591729dd780c9f1e58e01483b658e3ee015
abel verbo <havelinux@gmail.com>
2018-04-08 17:45:41 +0200

    add query across a join

--- src/AppBundle/Repository/GenusRepository.php
+++ src/AppBundle/Repository/GenusRepository.php
@@ -21,4 +21,18 @@ class GenusRepository extends EntityRepository
       ->execute();
   }
 
+  public function findAllPublishedOrderedByRecentlyActive()
+  {
+    /**
+     * @return Genus[]
+     */
+    return $this->createQueryBuilder('genus')
+      ->andWhere('genus.isPublished = :isPublished')
+      ->setParameter('isPublished', true)
+      ->leftJoin('genus.notes', 'genus_note')
+      ->orderBy('genus_note.createdAt', 'DESC')
+      ->getQuery()
+      ->execute();
+  }
+
 }
