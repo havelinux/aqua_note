4fe09dc453fdf1f406bf1ea3a748cdcbbcb97db8
abel verbo <havelinux@gmail.com>
2018-05-02 18:48:16 +0200

    register service in container

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -72,9 +72,7 @@ class GenusController extends Controller
       throw $this->createNotFoundException('No genus found.');
     }
 
-    $transformer = new MarkdownTransformer(
-      $this->get('markdown.parser')
-    );
+    $transformer = $this->get('app.markdown_transformer');
     $funFact = $transformer->parse($genus->getFunFact());
 
 //    $notes = [
